26faa78c905f96ccbfcceda9d8809abbc8843496
Xavier Miles <xmiles99@gmail.com>
2026-05-10 12:52:16 -0400

    Major sweep through of the whole project.
    Added global skip link.  Reworked About.html hero and claified the structure.
    Added lightbox to Gallery.html for image expansion. Improved feedback for Contact.html form.
    Streamlined navigation bar. Improved formatting on services.html and testimonials.html.
    Complete cleanup of styles.css - improved consistency of padding and sizing throughout the site. Removed some redundant rules.

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const toggle = document.getElementById("mobileToggle");
 const mobileNav = document.getElementById("mobileNav");
 const header = document.querySelector("header")
 
-// listening to the burger [menu button] to open/close it and changes from ☰ to X
+// listening to the burger [menu button] to open/close it and change from ☰ to X
 toggle.addEventListener("click", () => {
   toggleMobile();
 });
@@ -123,111 +123,7 @@ if (catFactBtn) {
   });
 }
 
-//contact form functionality. Basically, when the form is submitted, the "submit" button's appearance will change to have a loading circle and say "Sending..." instead, and will be disabled, and then after a brief delay the *actual* submit will occur
-const contactForm = document.getElementById("contactForm");
-const contactSubmit = document.getElementById("contactSubmit");
-const contactSubmitText = document.getElementById("contactSubmitText");
-const contactSpinner = document.getElementById("contactSpinner");
 
-const contactName = document.getElementById("contactName");
-const contactContact = document.getElementById("contactContact");
-const contactMessage = document.getElementById("contactMessage");
-
-const nameError = document.getElementById("nameError");
-const contactError = document.getElementById("contactError");
-const messageError = document.getElementById("messageError");
-
-const messageLength = document.getElementById("messageLength");
-
-let submitInProgress = false;
-
-if (contactForm) {
-  contactMessage.addEventListener("input", () => {
-    const length = contactMessage.value.length;
-    
-    if (length > 400 || length < 30) {
-      messageLength.style.color = "#6a0f0f";
-    } else if (length > 350) {
-      messageLength.style.color = "#7a3f00"; 
-    } else messageLength.style.color = "#013651";
-
-    messageLength.textContent = `${length} / 400`;
-  })
-  
-  contactForm.addEventListener("submit", (event) => {
-    //don't submit lol
-    event.preventDefault();
-
-    let isFormValid = true;
-
-    //reset errors if there are any 
-    nameError.textContent = "";
-    contactError.textContent = "";
-    messageError.textContent = "";
-    messageLength.style.color = "#013651";
-
-    //check if name is blank
-    if (contactName.value == "") {
-      isFormValid = false;
-      nameError.textContent = "Name cannot be blank";
-    }
-
-    // I used AI for the regex pattern of Email OR phone number xxxxxxxxx OR xxx-xxx-xxxx
-    // Check if contact info is blank or doesn't match accepted formats
-    const contactRegex = /^(?:[^\s@]+@[^\s@]+\.[^\s@]+|\d{10}|\d{3}-\d{3}-\d{4})$/;
-    if (contactContact.value == "") {
-      isFormValid = false;
-      contactError.textContent = "Contact information cannot be blank";
-    } else if (!contactRegex.test(contactContact.value)) {
-      isFormValid = false;
-      contactError.textContent = "Input valid email, or phone as 1234567890 or 123-456-7890"
-    }
-
-    //check if message is empty, too short, or too long
-    if (contactMessage.value == "") {
-      isFormValid = false;
-      messageError.textContent = "Please enter a message - include start date, duration needed and anything relevant!";
-    } else if (contactMessage.value.length < 30) {
-      isFormValid = false;
-      messageError.textContent = "Please enter more information - it helps me understand your needs and serve you better!";
-    } else if (contactMessage.value.length > 400) {
-      isFormValid = false;
-      messageError.textContent = "Message too long."
-    }
-
-    //check validity!
-    if (!isFormValid) return;
-
-    //don't do a resubmit if it's already happening
-    if (submitInProgress) return;
-
-    submitInProgress = true;
-
-    contactSubmitText.textContent = "Sending...";
-    contactSpinner.style.display = "inline-block";
-    contactSubmit.disabled = true;
-
-    //*actually* submit the form after 700ms
-    setTimeout(() => {
-      contactForm.submit();
-    }, 700);
-  })
-}
-
-// Receive data from URL passed from Services page booking click
-
-/* I used AI to figure out how to target the clicked service and send that to a query param.
-I knew I had to pass data from one page to another and knew how to select the checkbox on the destination page (contact.html) but I didn't know about data-service or how to use it, or about URLSearchParams specifically */
-const params = new URLSearchParams(window.location.search);
-const selectedService = params.get("service");
-
-if (selectedService) {
-  const checkbox = document.querySelector(`input[name="serviceType[]"][value="${selectedService}"]`);
-
-  if (checkbox) {
-    checkbox.checked = true;
-  }
-}
 
 //Services page functionality
 const bgMute = document.getElementById("bgMute");
@@ -257,11 +153,56 @@ if (bgMute) {
   });
 }
 
+
+
+//Gallery page functionality
+const galleryItems = document.querySelectorAll(".galleryItem img");
+
+//build the lightbox
+const lightbox = document.createElement("div");
+const lightboxImage = document.createElement("img");
+const lightboxClose = document.createElement("button");
+
+// let galleryImages = document.querySelectorAll("img");
+
+lightbox.id = "lightbox";
+
+lightboxClose.id = "lightboxClose";
+lightboxClose.textContent = "✖";
+
+document.body.appendChild(lightbox);
+lightbox.appendChild(lightboxClose);
+lightbox.appendChild(lightboxImage);
+
+//trigger the lightbox on gallery image click
+galleryItems.forEach((image) => {
+  image.addEventListener("click", () => {
+    lightboxImage.src = image.src;
+    lightboxImage.alt = image.alt;
+    lightbox.classList.toggle('active');
+  });
+});
+
+//close lightbox when you click outside of the image or press Esc
+lightbox.addEventListener("click", (event) => {
+  if (!lightboxImage.contains(event.target)) {
+    lightbox.classList.toggle("active");
+  }
+})
+
+document.addEventListener("keydown", (event) => {
+  if (event.keyCode === 27 && lightbox.classList.contains("active")) {
+    lightbox.classList.toggle("active");
+  }
+});
+
+
+
 // Testimonials page
-const testimonialQuote = document.querySelectorAll(".testimonial-quote");
+const testimonialQuotes = document.querySelectorAll(".testimonial-quote");
 
 //expands the quote, and changes the text to signal you can click again to collapse
-testimonialQuote.forEach((quote) => {
+testimonialQuotes.forEach((quote) => {
   quote.addEventListener("click", expandQuote);
 });
 
@@ -273,4 +214,120 @@ function expandQuote() {
   if (this.classList.contains("expanded")) {
     quoteButton.textContent = "\u00A0[Click to collapse]";
   } else quoteButton.textContent = "\u00A0[Click for full quote]";
+}
+
+
+
+//contact form functionality. Basically, when the form is submitted, the "submit" button's appearance will change to have a loading circle and say "Sending..." instead, and will be disabled, and then after a brief delay the *actual* submit will occur
+const contactForm = document.getElementById("contactForm");
+const contactSubmit = document.getElementById("contactSubmit");
+const contactSubmitText = document.getElementById("contactSubmitText");
+const contactSpinner = document.getElementById("contactSpinner");
+
+const contactName = document.getElementById("contactName");
+const contactContact = document.getElementById("contactContact");
+const contactMessage = document.getElementById("contactMessage");
+
+const nameError = document.getElementById("nameError");
+const contactError = document.getElementById("contactError");
+const serviceError = document.getElementById("serviceError");
+const messageError = document.getElementById("messageError");
+
+const messageLength = document.getElementById("messageLength");
+
+let submitInProgress = false;
+
+if (contactForm) {
+  //character counter
+  contactMessage.addEventListener("input", () => {
+    const length = contactMessage.value.length;
+    
+    if (length > 400 || length < 30) {
+      messageLength.style.color = "#6a0f0f";
+    } else if (length > 350) {
+      messageLength.style.color = "#7a3f00"; 
+    } else messageLength.style.color = "#013651";
+
+    messageLength.textContent = `${length} / 400`;
+  })
+  
+  contactForm.addEventListener("submit", (event) => {
+    //don't submit lol
+    event.preventDefault();
+
+    let isFormValid = true;
+
+    //reset errors if there are any 
+    nameError.textContent = "";
+    contactError.textContent = "";
+    serviceError.textContent = "";
+    messageError.textContent = "";
+    messageLength.style.color = "#013651";
+
+    //check if name is blank
+    if (contactName.value == "") {
+      isFormValid = false;
+      nameError.textContent = "Name cannot be blank.";
+    }
+
+    // I used AI for the regex pattern of Email OR phone number xxxxxxxxx OR xxx-xxx-xxxx
+    // Check if contact info is blank or doesn't match accepted formats
+    const contactRegex = /^(?:[^\s@]+@[^\s@]+\.[^\s@]+|\d{10}|\d{3}-\d{3}-\d{4})$/;
+    if (contactContact.value == "") {
+      isFormValid = false;
+      contactError.textContent = "Contact information cannot be blank.";
+    } else if (!contactRegex.test(contactContact.value)) {
+      isFormValid = false;
+      contactError.textContent = "Input valid email, or phone as 1234567890 or 123-456-7890."
+    }
+
+    //Ensure at least one service is checked
+    if (document.querySelectorAll('input[name="serviceType[]"]:checked').length == 0) {
+      isFormValid = false;
+      serviceError.textContent = "Please select at least one service type. If you don't see specifically what you're looking for, select Other.";
+    }
+
+    //check if message is empty, too short, or too long
+    if (contactMessage.value == "") {
+      isFormValid = false;
+      messageError.textContent = "Please enter a message - include start date, duration needed and anything relevant!";
+    } else if (contactMessage.value.length < 30) {
+      isFormValid = false;
+      messageError.textContent = "Please enter more information - it helps me understand your needs and serve you better!";
+    } else if (contactMessage.value.length > 400) {
+      isFormValid = false;
+      messageError.textContent = "Message too long."
+    }
+
+    //check validity!
+    if (!isFormValid) return;
+
+    //don't do a resubmit if it's already happening
+    if (submitInProgress) return;
+
+    submitInProgress = true;
+
+    contactSubmitText.textContent = "Sending...";
+    contactSpinner.style.display = "inline-block";
+    contactSubmit.disabled = true;
+
+    //*actually* submit the form after 700ms
+    setTimeout(() => {
+      contactForm.submit();
+    }, 200);
+  })
+}
+
+// Receive data from URL passed from Services page booking click
+/* I used AI to figure out how to target the clicked service and send that to a query param.
+I knew I had to pass data from one page to another and knew how to select the checkbox on the destination page (contact.html) but I didn't know about data-service or how to use it, or about URLSearchParams specifically */
+const params = new URLSearchParams(window.location.search);
+const selectedService = params.get("service");
+
+if (selectedService) {
+  const checkbox = document.querySelector(`input[name="serviceType[]"][value="${selectedService}"]`);
+
+  if (checkbox) {
+    checkbox.checked = true;
+  }
 }
